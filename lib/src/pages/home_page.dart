d36faced1d4f6a8d899cb044ec388912f2e25db6
Alan Barrera <aalan.eduardo@gmail.com>
2020-08-19 23:55:05 -0500

    20agost

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+import 'package:qrreaderapp/src/pages/mapas_pages.dart';
+import 'package:qrreaderapp/src/pages/direcciones_page.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('QR Scanner'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.delete_forever), onPressed: () {})
+        ],
+      ),
+      body: Center(child: _callPage(currentIndex)),
+      bottomNavigationBar: _crearBottonNavBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.filter_center_focus),
+        onPressed: () {},
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+    );
+  }
+
+  Widget _callPage(int paginaActual) {
+    switch (paginaActual) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
+
+      default:
+        return MapasPage();
+    }
+  }
+
+  Widget _crearBottonNavBar() {
+    return BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.map), title: Text('Map')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.arrow_drop_down_circle),
+              title: Text('Direcciones')),
+        ]);
+  }
+}
